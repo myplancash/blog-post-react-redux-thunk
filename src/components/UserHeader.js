debf2a354fa209f01be7017c8d1c86fb7006b2ae
myplancash <myplancash@gmail.com>
2021-10-29 16:21:13 -0500

    vscode inside gitignore

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -10,7 +10,7 @@ class UserHeader extends React.Component {
 
   render() {
 
-    this.props.users.find( (user) => user.id === this.props.userId)
+    this.props.users.find(user => user.id === this.props.userId)
 
     if (!user) {
       return null;
@@ -19,13 +19,6 @@ class UserHeader extends React.Component {
       <div>UserHeader</div>
     );
   }
-}
-
-
-if (tipoDeSuscripcion === 'free') {
-  return console.log("Solo puedes tomar los cursos gratis");
-} else if (tipoDeSuscripcion === "Basic") {
-   return console.log("Puedes tomar casi todos los cursos de Platzi durante un mes");
 }
 
 export default connect(null, { fetchUser })(UserHeader);
